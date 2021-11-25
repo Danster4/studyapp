a100b8d2459c1fedf381ac3ada0205d16084cf28
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-24 20:02:15 -0500

    User model has been finished. Still need to setup routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// activate routes
+app.use(routes);
+
+
+// turn on connection to db and server 
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
