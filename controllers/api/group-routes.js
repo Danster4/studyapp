21062630e385e2c5ece0c80853e12290f90c5d10
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-30 20:20:40 -0500

    Added get by id and put for Groups

--- controllers/api/group-routes.js
+++ controllers/api/group-routes.js
@@ -24,10 +24,6 @@ router.get('/', (req, res) => {
                     {
                         model: User,
                         attributes: ['username']
-                    },
-                    {
-                        model: Group,
-                        attributes: ['group_name']
                     }
                 ]
             }
@@ -40,12 +36,77 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/:id', (req, res) => {
+    Group.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'group_name'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Post,
+                attributes: [
+                    'id', 
+                    'post_title',
+                    'post_body',
+                    'created_at'
+                ],
+                include:[
+                    {
+                        model: User,
+                        attributes: ['username']
+                    }
+                ]
+            }
+        ]
+    })
+        .then(dbGroupData => {
+            if (!dbGroupData) {
+                res.status(404).json({ message: "No group found with this id."});
+                return;
+            }
+
+            res.json(dbGroupData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 router.post('/', (req, res) => {
     Group.create({
         group_name: req.body.group_name,
         user_id: req.body.user_id
     })
         .then(dbGroupData => res.json(dbGroupData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/:id', (req, res) => {
+    Group.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbGroupData => {
+            if (!dbGroupData) {
+                res.status(404).json(dbGroupData);
+                return;
+            }
+
+            res.json(dbGroupData);
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
