d84cd44e045dc5417e00f7712171f9685371ea69
Chris Zichko <chriszichkocoding@gmail.com>
2021-12-01 21:48:12 -0500

    Not yet able to load in data from db using handlebars.

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Group, Post, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_title',
+            'post_body',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Group,
+                attributes: ['group_name']
+            },
+            {
+                model: Comment,
+                attributes: ['comment_text', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // serialize data before passing to template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
