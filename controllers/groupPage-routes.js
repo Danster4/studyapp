a6ff3990f679b67f53333bd16fa431ee876e4935
Chris Zichko <chriszichkocoding@gmail.com>
2021-12-01 20:46:41 -0500

    The specific group page is rendering but not with the data.

---
+++ controllers/groupPage-routes.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Group, Post, Comment } = require('../models');
+
+router.get('/:id', (req, res) => {
+    Group.findOne({
+        attributes: [
+            'id',
+            'group_name'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Post,
+                attributes: [
+                    'id', 
+                    'post_title',
+                    'post_body',
+                    'created_at'
+                ],
+                include:[
+                    {
+                        model: User,
+                        attributes: ['username']
+                    },
+                    {
+                        model: Comment,
+                        attributes: [
+                            'comment_text',
+                            'created_at'
+                        ], 
+                        include: [
+                            {
+                                model: User,
+                                attributes: ['username']
+                            },
+                        ]
+                    }
+                ]
+            }
+        ]
+    })
+        .then(dbGroupData => {
+            if(!dbGroupData) {
+                res.status(404).json({ message: "No group found with this id." });
+                return
+            }
+            // const posts = dbGroupData.map(post => post.get({ plain: true }));
+            res.render('group');
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
+//     User.findAll({
+//         attributes: [
+//             'username'
+//         ]
+//     })
+//         .then(dbUserData => {
+//             const users = dbUserData.map(user => user.get({ plain: true }));
+//             res.render('group', { users });
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         });
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 
+const groupRoutes = require('./groupPage-routes');
 const homeRoutes = require('./home-routes.js');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/group/', groupRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
